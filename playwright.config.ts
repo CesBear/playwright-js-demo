2a5ad17ce8fd32214a45a1a7b203ca3e27f39737
Cesar Ramirez <carr.gdl1@gmail.com>
2022-12-05 20:44:22 -0600

    final conf

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,41 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-testDir: './tests',
-timeout: 30 * 10000,
-expect: {
-    timeout: 5000
-},
-reporter: 'html',
-use: {
-    actionTimeout: 0,
-    trace: 'on-first-retry',
-    extraHTTPHeaders: {
-        'Content-Type': 'application/json',
-      },
-},
+    testDir: './tests',
+    timeout: 30 * 10000,
+    expect: {
+        timeout: 5000
+    },
+    reporter: 'html',
+    use: {
+        headless: false,
+        actionTimeout: 0,
+        trace: 'on-first-retry',
+        extraHTTPHeaders: {
+            'Content-Type': 'application/json',
+        },
+    },
+    // below configuration will run parallel test
+    projects: [
+        {
+        name: 'chromium',
+        use: { ...devices['Desktop Chrome'] },
+        },
+        {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+        },
+        {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+        },
+        {
+        name: 'mobile-safari',
+        use: {
+            ...devices['iPad Pro'],
+        },
+        },
+    ],
 };
 
 export default config;
--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -4,11 +4,13 @@ const SPECIES = '/species';
 const STARWARSAPI = 'https://swapi.dev/api';
 
 test.describe('Demo API Test', () => {
-    test('should get Droid specie from Star Wars', async ({ request }) => {
+    test('should get droid and human species from Star Wars', async ({ request }) => {
         const getSpecies: any = await request.get(`${STARWARSAPI}${SPECIES}`);
         const res = await getSpecies.json();
         console.log("Starwar Specie name: ", res.results[1].name);
-
+        console.log("Starwar Specie name: ", res.results[0].name);
+        expect(res.results[0].name).toBe("Human");
         expect(res.results[1].name).toBe("Droid");
     });
-}); 
+});
+
